994e37a1cbef5540a3f434468d24e87502be3696
endorphin82 <endorphin82@gmail.com>
2020-11-17 17:08:46 +0200

    NotFound

--- pages/404.js
+++ pages/404.js
@@ -1,5 +1,10 @@
+import Link from "next/link"
+
 export default function NotFound() {
   return (
-    <h1> NotFound <strong>404</strong></h1>
+    <>
+      <h1> NotFound <strong>404</strong></h1>
+      <Link href={"/"}><a>Home</a></Link>
+    </>
   )
 }
