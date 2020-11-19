423b8cea36d307c4187769a7cfc6de40f8841252
endorphin82 <endorphin82@gmail.com>
2020-11-19 11:41:20 +0200

    Security Services

---
+++ pages/security-services/index.tsx
@@ -0,0 +1,168 @@
+import TitleOfAnyPage from "../../components/TitleOfAnyPage"
+import RightSidebar from "../../components/RightSidebar"
+import { MainLayout } from "../../components/MainLayout"
+
+export default function SecurityServices() {
+  return (
+    <MainLayout>
+      <TitleOfAnyPage />
+      <div className="row">
+        <div className="left-sidebar">
+
+          <div id="post-4187" className="page post-4187 type-page status-publish hentry">
+            <h4 className="custom-h4">We help businesses control the cyber resilience of their it resources against
+              malicious attacks</h4>
+            <h5><strong>Did you know that</strong></h5>
+            <p></p>
+            <p className="custom-text-os">Only <span className="bold__numbers">10 %</span> of organizations globally
+              told they felt "very confident" about data security </p>
+            <p className="custom-text-os"><span className="bold__numbers">44 %</span> of organizations said they
+              suffered a successful phishing attack in 2018 — up from 30 % in 2017.</p>
+            <p className="custom-text-os"><span className="bold__numbers">54 %</span> of firms had data breaches in 2018
+              involving the theft of customer and employee information.</p>
+            <p className="custom-text-os"><span className="bold__numbers">61 %</span> of SMBs experienced a cyber-attack
+              in the last 12 months of 2018, up from <span className="bold__numbers">55 %</span> in 2017<br />
+              Each organization faced an average of <span className="bold__numbers">11.4</span> successful phishing
+              attacks in 2018 Every <span className="bold__numbers">39</span> seconds a hacker attacks an
+              internet-connected computer</p>
+            <h5 className="mt25"><strong>Who We Are</strong></h5>
+            <p></p>
+            <p className="custom-text-os">We are a cybersecurity company comprised of young and experienced
+              professionals who have completed 105 security projects for customers from 15 countries in 2018. Our
+              security experts possess the following certifications: <span className="bold__numbers transparent">CEH v9, ISO 27001 LA, OSCP, HIPPA, CCNA, eWPT and eCPPT</span>.
+            </p>
+            <p className="custom-text-os"><span className="bold__numbers">Our industries:</span></p>
+            <ul className="fa-ul custom-icon-ul">
+              <li>
+                <i className="fa-li fa fa-cogs fa-lg"></i><p></p>
+                <p className="custom-text-os">Software Development</p>
+              </li>
+              <li>
+                <i className="fa-li fa fa-cloud fa-lg"></i><p></p>
+                <p className="custom-text-os">Hosting Providers</p>
+              </li>
+              <li>
+                <i className="fa-li fa fa-refresh fa-lg"></i><p></p>
+                <p className="custom-text-os">Logistics</p>
+              </li>
+
+              <li>
+                <i className="fa-li fa fa-shopping-cart fa-lg"></i><p></p>
+                <p className="custom-text-os">E-commerce</p>
+              </li>
+
+              <li>
+                <i className="fa-li fa fa-money fa-lg"></i><p></p>
+                <p className="custom-text-os">Fintech</p>
+              </li>
+              <li>
+                <i className="fa-li fab fa-btc fa-lg"></i><p></p>
+                <p className="custom-text-os">Blockchain</p>
+              </li>
+              <li>
+                <i className="fa-li fa fa-gamepad fa-lg"></i><p></p>
+                <p className="custom-text-os">Online Gaming</p>
+              </li>
+              <li>
+                {/*<i className="fa-li fab fa-file-text-o fa-lg"></i><p></p>*/}
+                <i className="fa-li fas fa-file-alt fa-lg"></i><p></p>
+                <p className="custom-text-os">News Sites and Media Publications</p>
+              </li>
+              <li>
+                <i className="fa-li fa fa-credit-card fa-lg"></i><p></p>
+                <p className="custom-text-os">Financial Services</p>
+              </li>
+            </ul>
+            <h4 className="custom-h4">Take a look at the statistics</h4>
+            <p><img className="alignnone size-full wp-image-2472"
+                    src="/assets/imgs/security-services/statistic.jpg" /><br />
+              Are you sure you are not here either?_</p>
+            <h5>We are good at:</h5>
+            <h4 className="custom-h4"><strong>PENETRATION TESTING</strong></h4>
+            <p className="custom-text-os">Our security experts act out the role of a hacker, trying to compromise IT
+              systems and employees to discover any potential weaknesses that could be exploited by real hackers. We
+              collect the results of our simulated hacker attacks, summarize them and present our findings in a
+              high-level report that contains an executive summary, found vulnerabilities ranged according to CVSS v3
+              including our specific recommendations for fixing them.</p><p></p>
+            <p className="custom-text-os"><span className="bold__numbers">Penetration Testing Services:</span> web, iOS,
+              Android</p>
+            <p className="custom-text-os"><span className="bold__numbers">Deliverables:</span> Consultant Technical
+              Report with detailed findings Remediation recommendations and technical references</p>
+            <p className="custom-text-os"><span className="bold__numbers">Network &amp; System Testing:</span></p>
+            <p className="custom-text-os">Our approach is to identify the most serious risks and security flaws first
+              and then focus on the less obvious areas as the project proceeds. Firstly, we test the network for
+              vulnerabilities from the outside, conducting the test from the point of view of an uninformed attacker. We
+              then gradually increase the amount of information given to our testers until they assume the role of a
+              trusted user of the network trying to access an unauthorized resource or service.</p>
+            <p className="custom-text-os"><span className="bold__numbers">Deliverables:</span></p>
+            <p className="custom-text-os">Consultant Technical Report with a detailed findings section. Screenshots or a
+              detailed description regarding the reproduction of security issues Vulnerabilities ranked by Risk level,
+              CWE, CVSS v3.0.</p>
+            <hr />
+            <h4 className="custom-h4"><strong>SMART CONTRACT AUDIT</strong></h4>
+            <p className="custom-text-os">A Smart Contract Audit evaluates the overall architecture and design
+              choices, as well as whether the code has been written according to the most up to date and established
+              practices for Smart Contract development.</p>
+            <p className="custom-text-os">We have embraced new and trending technologies and have joined the community
+              of blockchain auditing companies to offer our expertise in auditing smart contracts on the Ethereum, EOS
+              and TRON networks.</p>
+            <p className="custom-text-os"><span className="bold__numbers">Deliverables:</span></p>
+            <p className="custom-text-os">Report with an as-is overview, executive summary and found vulnerabilities
+              along with our recommendations on how to fix them.</p>
+            <hr />
+            <h4 className="custom-h4"><strong>COMPANY SECURITY AUDIT</strong></h4>
+            <p className="custom-text-os">A company security audit is a set of tools and procedures which check that
+              the architecture and implementation of the IT systems in your organization are resistant to malicious
+              software, black hat hackers, DoS / DDoS attacks. They also ensure your employees are aware of how to
+              avoid and deal with phishing attacks and social engineering techniques.</p>
+            <p className="custom-text-os"><span className="bold__numbers">This service includes the following components:</span>
+            </p>
+            <ul className="fa-ul custom-icon-ul">
+              <li>
+                <i className="fa-li fa fa-desktop fa-lg"></i><p></p>
+                <p className="custom-text-os">Network security assessment, including Wi-Fi network</p>
+              </li>
+              <li>
+                <i className="fa-li fa fa-check fa-lg"></i><p></p>
+                <p className="custom-text-os">Web security assessment</p>
+              </li>
+              <li>
+                <i className="fa-li fa fa-refresh fa-lg" aria-hidden="true"></i><p></p>
+                <p className="custom-text-os">Load Testing
+                </p>
+              </li>
+              <li>
+                <i className="fa-li fas fa-tachometer-alt fa-lg"></i><p></p>
+                <p className="custom-text-os">Performance Testing</p>
+              </li>
+              <li>
+                <i className="fa-li fa fa-bolt fa-lg"></i><p></p>
+                <p className="custom-text-os">DoS / DDoS testing</p>
+              </li>
+              <li>
+                <i className="fa-li fa fa-code fa-lg"></i><p></p>
+                <p className="custom-text-os">Phishing simulation</p>
+              </li>
+              <li>
+                <i className="fa-li fa fa-users fa-lg"></i><p></p>
+                <p className="custom-text-os">Social engineering testing</p>
+              </li>
+            </ul>
+            <p className="custom-text-os"><span className="bold__numbers">Key Objectives of Service</span></p>
+            <p className="custom-text-os">To implement basic control measures for operations of information security
+              in the organization. Protect the organization against security breaches and data leaks. Ensure the
+              information and information processing tools are secure and protected from cybercriminals and black
+              hat hackers according to the best practice experience. Prevent exploitation of technical
+              vulnerabilities and logic errors in IT systems.</p>
+            <p className="custom-text-os">Prevent exploitation of security vulnerabilities in IT systems. Log
+              events, provide evidence, report identified issues and present remediation recommendations.</p>
+            <div className="clear"></div>
+
+          </div>
+
+        </div>
+        <RightSidebar />
+      </div>
+    </MainLayout>
+  )
+}
