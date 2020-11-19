efb701e10b470f743ef1e0a58adbd66f805c5f8f
endorphin82 <endorphin82@gmail.com>
2020-11-19 17:13:36 +0200

    magento-2

---
+++ pages/magento-2/index.tsx
@@ -0,0 +1,116 @@
+import TitleOfAnyPage from "../../components/TitleOfAnyPage"
+import RightSidebar from "../../components/RightSidebar"
+import { MainLayout } from "../../components/MainLayout"
+
+export default function Magento2() {
+  return (
+    <MainLayout>
+      <TitleOfAnyPage />
+      <div className="row">
+        <div className="left-sidebar">
+
+          <div id="post-2151" className="page post-2151 type-page status-publish hentry">
+            <h4 className="custom-h4">Better. Faster. Safer.</h4>
+            <p className="custom-text-os">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Stable and highly advanced solution for
+              your e-commerce business<br/>
+                Magento 2 provides new ways to improve the user experience, navigation, income generation and growth of
+                your business.</p>
+            <p></p>
+            <ul id="ul-li-arrow" className="fa-ul custom-icon-ul ul-li-arrow">
+              <li>
+                <p className="custom-text-os"><a
+                  href="https://m-integration.com/advanced-marketing-seo-product-management-optimization-tools/">Advanced
+                  marketing, SEO, product management and optimization tools</a></p>
+              </li>
+              <li>
+                <p className="custom-text-os"><a
+                  href="https://m-integration.com/improved-performance-security-flexibility/">Improved performance,
+                  security, and flexibility</a></p>
+              </li>
+              <li>
+                <p className="custom-text-os"><a href="https://m-integration.com/updated-technology-stack/">Updated
+                  technology stack</a></p>
+              </li>
+              <li>
+                <p className="custom-text-os"><a href="https://m-integration.com/open-flexible-architecture/">Open,
+                  Flexible Architecture</a></p>
+              </li>
+              <li>
+                <p className="custom-text-os"><a href="https://m-integration.com/easier-maintenance-upgrades/">Easier
+                  Maintenance and Upgrades</a></p>
+              </li>
+              <li>
+                <p className="custom-text-os"><a href="https://m-integration.com/new-file-structure/">New File
+                  Structure</a></p>
+              </li>
+              <li>
+                <p className="custom-text-os"><a href="https://m-integration.com/testing-built-testing-framework/">Testing
+                  the Built In Testing Framework</a></p>
+              </li>
+              <li>
+                <p className="custom-text-os"><a href="https://m-integration.com/excellent-compatibility-platforms/">Excellent
+                  compatibility with other platforms</a></p>
+              </li>
+              <li>
+                <p className="custom-text-os"><a
+                  href="https://m-integration.com/performance-improvements-faster-page-loads-25-faster-browsing/">Performance
+                  Improvements and speeds up page loads for 25% faster browsing</a></p>
+              </li>
+              <li>
+                <p className="custom-text-os"><a href="https://m-integration.com/new-secure-payments/">New Secure
+                  Payments</a></p>
+              </li>
+              <li>
+                <p className="custom-text-os"><a href="https://m-integration.com/caching-optimization/">Caching
+                  optimization</a></p>
+              </li>
+              <li>
+                <p className="custom-text-os"><a href="https://m-integration.com/new-improved-modern-default-theme/">New
+                  improved modern default theme</a></p>
+              </li>
+              <li>
+                <p className="custom-text-os"><a href="https://m-integration.com/open-source-community/">Open source
+                  community</a></p>
+              </li>
+              <li>
+                <p className="custom-text-os"><a
+                  href="https://m-integration.com/import-export-functionality-faster-advanced/">The import and export
+                  functionality is faster and it is more advanced</a></p>
+              </li>
+              <li>
+                <p className="custom-text-os"><a href="https://m-integration.com/responsive-design/">Responsive
+                  design</a></p>
+              </li>
+              <li>
+                <p className="custom-text-os"><a
+                  href="https://m-integration.com/intuitive-back-end-admin-ux-framework/">Intuitive back-end Admin UX
+                  and Framework</a></p>
+              </li>
+              <li>
+                <p className="custom-text-os"><a href="https://m-integration.com/use-tablet-manage-store/">Use a tablet
+                  to manage your store</a></p>
+              </li>
+              <li>
+                <p className="custom-text-os"><a href="https://m-integration.com/ability-add-videos/">Ability to add
+                  videos</a></p>
+              </li>
+            </ul>
+            <p>&nbsp;</p>
+            <h4 className="custom-h4">Technology stack of requirements:</h4>
+            <p><img className="alignnone size-full wp-image-2340"
+                    src="/assets/imgs/magento-2/tech-stack-min.png"
+                    alt="Technology stack"/></p>
+            <p className="custom-text-os">* <i>RequireJS, Require up to 2GB of RAM</i></p>
+            <p>&nbsp;</p>
+            <p className="custom-text-os upper"><b>Magento is the most popular e-commerce platform in the world, with
+              more than 250,000 online merchants around the globe.</b></p>
+            <div className="clear"></div>
+
+          </div>
+
+        </div>
+        <RightSidebar />
+      </div>
+    </MainLayout>
+  )
+}
