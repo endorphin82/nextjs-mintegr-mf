fd23cd2d94699df3eb45204f84c16beba41781da
endorphin82 <endorphin82@gmail.com>
2020-11-25 14:20:19 +0200

    about slider done

--- pages/about2/index.tsx
+++ pages/about2/index.tsx
@@ -5,24 +5,31 @@ import Slider from "react-slick";
 
 export default function About() {
   const settings = {
+    slidesToShow: 3,
     dots: true
   };
   return (
     <MainLayout>
       <div id="about2" className="container">
         <Slider {...settings}>
-          <div>
-            <img src="http://placekitten.com/g/400/200" />
-          </div>
-          <div>
-            <img src="http://placekitten.com/g/400/200" />
-          </div>
-          <div>
-            <img src="http://placekitten.com/g/400/200" />
-          </div>
-          <div>
-            <img src="http://placekitten.com/g/400/200" />
-          </div>
+          <div className="company-img"><img
+            src="https://m-integration.com/wp-content/uploads/2019/05/mariablackpng1.png" /></div>
+          <div className="company-img"><img
+            src="https://m-integration.com/wp-content/uploads/2019/05/olivellaline1.png" /></div>
+          <div className="company-img"><img
+            src="https://m-integration.com/wp-content/uploads/2019/05/Pagination1.png" /></div>
+          <div className="company-img"><img
+            src="https://m-integration.com/wp-content/uploads/2019/05/myfitaihi1.png" /></div>
+          <div className="company-img"><img
+            src="https://m-integration.com/wp-content/uploads/2019/05/wallebroek1.png" /></div>
+          <div className="company-img"><img
+            src="https://m-integration.com/wp-content/uploads/2019/05/beautifulonraw1.png" /></div>
+          <div className="company-img"><img
+            src="https://m-integration.com/wp-content/uploads/2019/05/restockchicago1.png" /></div>
+          <div className="company-img"><img
+            src="https://m-integration.com/wp-content/uploads/2019/05/kerluxe1.png" /></div>
+          <div className="company-img"><img
+            src="https://m-integration.com/wp-content/uploads/2019/05/bemaad1.png" /></div>
         </Slider>
       </div>
     </MainLayout>
